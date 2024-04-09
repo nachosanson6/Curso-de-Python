cdc209a452ffd9d52232bd0a656ec7cfcf642de8
nachosanson6 <nachosanson6@gmail.com>
2024-04-09 11:50:44 +0200

    variables

---
+++ 00_hello.py
@@ -0,0 +1,5 @@
+# Hola mundo
+print ('Hola Python')
+print(type(5.5))
+
+
---
+++ 01_variables.py
@@ -0,0 +1,8 @@
+my_variable ='hola que tal'
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
