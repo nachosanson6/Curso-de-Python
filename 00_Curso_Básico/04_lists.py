237ef8439e18606bc7a63dc0f0bdb263056f5894
nachosanson6 <nachosanson6@gmail.com>
2024-04-09 17:15:41 +0200

    lists

--- 00_Curso_Básico/04_lists.py
+++ 00_Curso_Básico/04_lists.py
@@ -1,6 +1,6 @@
 
 my_list = list()
-my_other_list = []
+my_other_list = [33, 1.78, 'Nacho', 'Sansón']
 
 print(len(my_list))
 
@@ -10,3 +10,17 @@ print(len(my_list))
 print(type(my_list))
 
 print(my_list[0])
+print(my_list[-1])
+print(my_list[-3])
+print(my_list.count(24))
+
+age, height, name, surname = my_other_list
+print(name)
+
+name, height, age, surname = my_other_list[2], my_other_list[1], my_other_list[0], my_other_list[3]
+print(name)
+
+print(my_list + my_other_list)
+
+my_list= 'Hola Python'
+print(my_list)
