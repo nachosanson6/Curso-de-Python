5b48685bcfc566c0c91aecd47faa9641aff2f391
nachosanson6 <nachosanson6@gmail.com>
2024-04-09 14:07:45 +0200

    operators

--- 00_Curso_Básico/03_strings.py
+++ 00_Curso_Básico/03_strings.py
@@ -10,3 +10,39 @@ print(my_new_line_string)
 
 my_new_tab_string = 'Este esu n string\tcon salta de linea'
 print(my_new_tab_string)
+
+#Formateo
+name = 'Nacho'
+surname = 'Sansón'
+age = 33
+
+print('Mi nombre es {} {} y mi edad es {}'.format(name,surname,age))
+print('Mi nombre es %s %s y mi edad es %d' %(name,surname,age))
+print(f'Mi nombre es {name} {surname} y mi edad es {age}')
+
+#Desempaquetado de caracteres
+'''language = 'Python'
+a , b = language
+print(a)
+print(b)'''
+
+language = 'Python'
+a , b, c, d, e, f = language
+print(a)
+print(b)
+
+#División
+
+language_slice = language[1:4]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+
+#Reverse
+
+reversed_language = language[:: -1] #Da la vuelta a la palabra
+print(reversed_language)
