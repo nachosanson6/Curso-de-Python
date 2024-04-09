fb4fc55ba5029e0494794f74eea7c90eaac09573
nachosanson6 <nachosanson6@gmail.com>
2024-04-09 12:04:38 +0200

    variables

--- 01_variables.py
+++ 01_variables.py
@@ -6,3 +6,12 @@ print(my_int_variable)
 
 my_bool_variable = False
 print(my_bool_variable)
+
+#Concatenación de variables
+print(my_variable, my_int_variable, my_bool_variable)
+#Cuanta el número de caracteres 
+print(len(my_variable))
+
+#Variables en una sola linea
+name,surname, alias, edad = 'Ignacio', 'Sansón', 'Nacho', 33
+print('Soy',name, surname,'tengo', edad, 'años', 'aunque todos me llaman' , alias)
