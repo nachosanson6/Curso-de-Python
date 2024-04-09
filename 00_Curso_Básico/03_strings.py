2274849eb4e372a1aef0af6c430e5df03f740f9c
nachosanson6 <nachosanson6@gmail.com>
2024-04-09 13:45:50 +0200

    operators

---
+++ 00_Curso_Básico/03_strings.py
@@ -0,0 +1,12 @@
+my_string = 'my string'
+my_other_string = 'my other string'
+
+print(len(my_string))
+print(len(my_other_string))
+print(my_string, 'and', my_other_string)
+
+my_new_line_string = 'Este esu n string\ncon salta de linea'
+print(my_new_line_string)
+
+my_new_tab_string = 'Este esu n string\tcon salta de linea'
+print(my_new_tab_string)
