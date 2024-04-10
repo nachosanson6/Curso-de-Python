64a86c9aa6dd8d11b3541b2a38bd862180f18a46
nachosanson6 <nachosanson6@gmail.com>
2024-04-10 10:14:10 +0200

    sets

--- 00_Curso_Básico/06_sets.py
+++ 00_Curso_Básico/06_sets.py
@@ -11,7 +11,16 @@ print(type(my_other_set))  #<class 'set'>
 print(len(my_other_set))
 print(my_other_set) #Un set no es una estructura ordenada
 
+
+# Funciones
 my_other_set.add('fgdgdf')
 print(my_other_set)
 my_other_set.add('fgdgdf')
 print(my_other_set)  # Si añades dos veces el mismo elemento solo lo almacena 1 sola vez.
+
+print('Sanson' in my_other_set)  # True.  Permite realizar búsquedas
+print('Sansin' in my_other_set)  #False
+
+my_other_set.remove(33)  # Permite eliminar el elemento que queramos
+print(my_other_set)
+
