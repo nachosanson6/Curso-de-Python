d6353d71b79535aae0aee1bb13e5f8f9d848e90f
nachosanson6 <nachosanson6@gmail.com>
2024-04-10 12:25:56 +0200

    conditionals

---
+++ 00_Curso_Básico/08_conditionals.py
@@ -0,0 +1,11 @@
+
+my_condition = 5*4
+
+if my_condition == 10:
+    print('Es igual a 10')
+
+elif my_condition > 10 and my_condition < 20:
+    print('Es mayor que 10 y menor que 20')
+else : 
+    print('No es 10 ni es maoyr que 10 y menor que 20')
+
