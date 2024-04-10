9f8c2d1b9937db2ff974e4c47b639f18c2967648
nachosanson6 <nachosanson6@gmail.com>
2024-04-10 13:05:03 +0200

    loops

--- 00_Curso_Básico/08_conditionals.py
+++ 00_Curso_Básico/08_conditionals.py
@@ -7,7 +7,7 @@ if my_condition == 10:
 elif my_condition > 10 and my_condition < 20:
     print('Es mayor que 10 y menor que 20')
 else : 
-    print('No es 10 ni es maoyr que 10 y menor que 20')
+    print('No es 10 ni es mayor que 10 y menor que 20')
 
 # En los condicionales de utilizan and, or y not
 
---
+++ 00_Curso_Básico/09_loops.py
@@ -0,0 +1,8 @@
+
+my_loop_condition = 0
+
+# While
+
+while my_loop_condition < 10:
+    print(my_loop_condition)
+    my_loop_condition += 1
