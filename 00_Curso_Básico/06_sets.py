18c307f1b38d139c9dd575f9d0303489ead941fd
nachosanson6 <nachosanson6@gmail.com>
2024-04-10 10:03:50 +0200

    sets

---
+++ 00_Curso_Básico/06_sets.py
@@ -0,0 +1,10 @@
+ 
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))  #<class 'set'>
+print(type(my_other_set))  #<class 'dict'>
+
+my_other_set = {'Nacho', 'Sanson', 33, 1.78}
+
+print(type(my_other_set))  #<class 'set'>
