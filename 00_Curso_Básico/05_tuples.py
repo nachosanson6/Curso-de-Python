7873d24ef82cf321ee2b1b219ce4599e7345165d
nachosanson6 <nachosanson6@gmail.com>
2024-04-10 09:24:18 +0200

    tuples

--- 00_Curso_Básico/05_tuples.py
+++ 00_Curso_Básico/05_tuples.py
@@ -1,3 +1,11 @@
 
 my_tuple = tuple()
 my_other_tuple = ()
+
+my_tuple = (33, 1.78, 'Nacho', 'Sanson')
+print(my_tuple)
+print(type(my_tuple))
+print(my_tuple[0])
+
+#Funciones
+print(my_tuple.count(33)) #Funciona como en las listas
