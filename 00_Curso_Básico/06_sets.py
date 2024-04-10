c5c164b4013eb2d49efe71f237f693a9675bd221
nachosanson6 <nachosanson6@gmail.com>
2024-04-10 10:08:57 +0200

    sets

--- 00_Curso_Básico/06_sets.py
+++ 00_Curso_Básico/06_sets.py
@@ -8,3 +8,10 @@ print(type(my_other_set))  #<class 'dict'>
 my_other_set = {'Nacho', 'Sanson', 33, 1.78}
 
 print(type(my_other_set))  #<class 'set'>
+print(len(my_other_set))
+print(my_other_set) #Un set no es una estructura ordenada
+
+my_other_set.add('fgdgdf')
+print(my_other_set)
+my_other_set.add('fgdgdf')
+print(my_other_set)  # Si añades dos veces el mismo elemento solo lo almacena 1 sola vez.
