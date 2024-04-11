bc9d23897706ff9a555abef5cfff10c80e96a6b0
nachosanson6 <nachosanson6@gmail.com>
2024-04-11 12:18:10 +0200

    exceptions

---
+++ 00_Curso_Básico/12_error_handling.py
@@ -0,0 +1,17 @@
+
+number_one = 5
+number_two = '2'
+
+try:
+    print(number_one + number_two)
+
+except:
+    print('Se ha producido un error')
+
+else: # Opcional
+    # Se ejecuta si no hay un error
+    print('La ejecución continúa')
+
+finally: # Opcional
+        # Se ejecuta siempre
+        print('La ejecución continúaaaaaa')
