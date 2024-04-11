32d74cffdb17a08ec70bb02d7d6073b30cd43d4f
nachosanson6 <nachosanson6@gmail.com>
2024-04-11 11:31:28 +0200

    classes

--- 00_Curso_Básico/11_classes.py
+++ 00_Curso_Básico/11_classes.py
@@ -6,20 +6,6 @@ class MyEmptyPerson :
 print(MyEmptyPerson())
 print(MyEmptyPerson)
 
-
-# Los parametros vienen desde fuera
-class Person :
-    def __init__(self,name,surname):
-        self.name = name
-        self.surname = surname
-        self.full_name = f'{name} {surname}'
-
-my_person = Person('Nacho','Sanson')
-
-print(my_person.name)
-print(f'{my_person.name} {my_person.surname}')
-print(my_person.full_name)
-
 # Los parametros vienen dados dentro del constructor
 class Person :
     def __init__(self):
@@ -30,3 +16,31 @@ my_person = Person()
 
 print(my_person.name)
 print(f'{my_person.name} {my_person.surname}')
+
+
+# Los parametros vienen desde fuera
+class Person :
+    def __init__(self,name,surname,alias = 'Sin alias'):
+        self.__name = name  # Con las dos barras bajas privatizo este dato y no se puede acceder a él
+        self.__surname = surname
+        self.alias = alias
+        self.full_name = f'{name} {surname} {alias}'
+
+    def get_name (self):
+        return self.__name
+    
+    def walk(self):
+        print(f'{self.full_name} está caminando')
+
+my_person = Person('Nuria','Soriano')
+
+# print(my_person.name)
+# print(f'{my_person.name} {my_person.surname}')
+print(my_person.full_name)
+my_person.walk()
+print(my_person.get_name())
+
+
+
+
+
