d274583349a27a06993e6014abe5e43d266c8062
nachosanson6 <nachosanson6@gmail.com>
2024-04-11 18:40:16 +0200

    dates

--- 01_Intermidiate/00_dates.py
+++ 01_Intermidiate/00_dates.py
@@ -3,10 +3,60 @@ from datetime import datetime
 
 now = datetime.now()
 
-print('year:',now.year)
-print('month:',now.month)
-print('day:',now.day)
-print('hour:',now.hour)
-print('minute:',now.minute)
-print('seconds:',now.second)
+def print_date(date):
+    print('year:',date.year)
+    print('month:',date.month)
+    print('day:',date.day)
+    print('hour:',date.hour)
+    print('minute:',date.minute)
+    print('seconds:',date.second)
+    print('timestamp', date.timestamp())
+
+print_date(now)
+
+year_2023 = datetime(2023,1,1)
+
+print_date(year_2023)
+
+
+
+from datetime import time
+
+current_time = time(21,6,10)
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+
+from datetime import date
+
+current_date = date.today()
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(2024,6,13)
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+
+# Operaciones con fechas
+
+diff = year_2023 - now
+print(diff)
+
+from datetime import timedelta
+
+start_time_delta = timedelta(200,100,100,weeks=10)
+end_time_delta = timedelta(300,100,100,weeks=13)
+
+print(end_time_delta - start_time_delta)
+print(end_time_delta + start_time_delta)
+
+
+
 
