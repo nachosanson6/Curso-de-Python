0ce18a12e31854295d1b5849b9ae413b38252d0a
nachosanson6 <nachosanson6@gmail.com>
2024-04-11 18:03:45 +0200

    dates

---
+++ 01_Intermidiate/00_dates.py
@@ -0,0 +1,12 @@
+
+from datetime import datetime
+
+now = datetime.now()
+
+print('year:',now.year)
+print('month:',now.month)
+print('day:',now.day)
+print('hour:',now.hour)
+print('minute:',now.minute)
+print('seconds:',now.second)
+
