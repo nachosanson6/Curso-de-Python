e3c21058db8877961fcfdbbd7eba11b0f56d03b0
nachosanson6 <nachosanson6@gmail.com>
2024-04-12 12:30:27 +0200

    lambdas

--- 01_Intermidiate/04_higher_order_functions.py
+++ 01_Intermidiate/04_higher_order_functions.py
@@ -1,4 +1,6 @@
 
+from functools import reduce
+
 def sum_one(value):
     return value + 1
 
@@ -19,5 +21,40 @@ print(sum_two_values_and_add(5,2, sum_one))
 print(sum_two_values_and_add(5,2, sum_five))
 
 
-# Closures
+### Closures
 
+def sum_ten():
+    def add(value):
+        return value + 10
+    return add
+
+add_closure = sum_ten()
+print(add_closure(5))
+
+### Built-in higher order functions
+numbers = [2, 5, 10, 21,3, 30, 5, 25, 95, 1, 87]
+# Map
+def multiply_two(number):
+    return number * 2
+
+print(list(map(multiply_two, numbers)))  #[4, 10, 20, 42]
+print(list(map(lambda number: number * 2, numbers)))  #[4, 10, 20, 42]
+
+# Filter
+
+def greater_than_ten(number):
+    if number > 10:
+        return True
+    return False
+
+print(list(filter(greater_than_ten,numbers)))
+print(list(filter(lambda number:number > 10,numbers)))
+
+# Reduce  Opera con un valor y el acumulado
+
+def sum_two_values(first_value,second_value):
+    print(first_value)
+    print(second_value)
+    return first_value + second_value
+
+print(reduce(sum_two_values,numbers))
