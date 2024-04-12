a30a60009c4538ba45f23a18c9ca2160a774ab69
nachosanson6 <nachosanson6@gmail.com>
2024-04-12 10:53:37 +0200

    challenges

--- 01_Intermidiate/02_challenges.py
+++ 01_Intermidiate/02_challenges.py
@@ -57,16 +57,39 @@ de Fibonacci empezando en 0.
   la que el siguiente siempre es la suma de los dos anteriores.
   0, 1, 1, 2, 3, 5, 8, 13...
 """
+def fibonacci():
 
+    prev = 0
+    next = 1
 
+    for element in range(0,50):
+        print(prev)
 
+        fib = prev + next
+        prev = next
+        next = fib
+
+fibonacci()
 
 """
 ¿ES UN NÚMERO PRIMO?
 Escribe un programa que se encargue de comprobar si un número es o no primo.
 Hecho esto, imprime los números primos entre 1 y 100.
 """
+def is_prime ():
+    for number in range(2,101):
+            
+        is_divisible = False
 
+        for element in range(2,number):
+            if number % element == 0:
+                 is_divisible = True 
+                 break  
+        
+        if not is_divisible:
+            print(number)
+
+is_prime()
 
 
 """
@@ -75,5 +98,14 @@ Crea un programa que invierta el orden de una cadena de texto
 sin usar funciones propias del lenguaje que lo hagan de forma automática.
 - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
 """
+def invert(text):
+
+    reversed_text = ''
+    text_len = len(text)
+
+    for index in range(0, text_len):
+        reversed_text += text[text_len - index - 1]
+    return reversed_text
 
 
+print(invert('hola mundo'))
