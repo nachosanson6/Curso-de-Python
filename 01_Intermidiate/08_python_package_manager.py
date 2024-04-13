f5c2267834745f03fa5034dc1c1e9fd1039d3265
nachosanson6 <nachosanson6@gmail.com>
2024-04-13 11:08:44 +0200

    backend course

--- 01_Intermidiate/08_python_package_manager.py
+++ 01_Intermidiate/08_python_package_manager.py
@@ -4,3 +4,18 @@
 import numpy
 
 print(numpy.version.version)
+
+array = numpy.array([35, 24, 60, 30, 56, 25, 78])
+print (type(array))
+
+print(array * 2)  # [ 70  48 120  60 112  50 156]
+
+import pandas
+
+### pip list
+### pip uninstall
+# pip show numpy
+
+import requests
+
+
